c8c69b717c54f71d00a7b4e7ffdc7b67c45506ad
gopichandana <gopichandanasiri@gmail.com>
2021-04-19 17:47:49 +0530

    Navbar done

---
+++ myportfolio/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+import MyNavbar from "./components/navbar"
+ const App=() => {
+  return (
+    <div >
+     <MyNavbar/>
+    </div>
+  );
+}
+
+export default App;
